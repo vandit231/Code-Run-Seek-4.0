70e0a92262d15ac133d0149794f1d144ee9ca3a3
saiharshavarma <saiharshavarma@outlook.com>
2022-09-07 22:27:53 +0530

    Homepage and Navbar integrated

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+document.querySelector(".icon").addEventListener("click", function () {
+  document.querySelector(".sidebar").classList.toggle("toggle2");
+  document.querySelector(".ias").classList.toggle("toggle1");
+});
+
